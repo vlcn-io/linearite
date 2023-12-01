e58a6bcfcf5651966eb56b7dcdef7fdd9c1c636e
Matt <1009003+tantaman@users.noreply.github.com>
2023-12-01 14:43:23 -0500

    add some todos to the readme

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ async function seedDB(db) {
   );
   db.transaction(() => {
     let i = 0;
-    for (const [issue, description] of createIssues(10000)) {
+    for (const [issue, description] of createIssues(7000)) {
       createIssueStmt.run(issue.id, issue.title, issue.creator, issue.priority, issue.status, issue.created, issue.modified, issue.kanbanorder);
       createDescriptionStmt.run(description.id, description.body);
     }
